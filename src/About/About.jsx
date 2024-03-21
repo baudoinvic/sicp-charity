ba2438306e258991846cf4b61d2be899f3ddd83d
baudoin <baudoinvicbolingo@gmail.com>
2024-03-21 13:18:44 +0200

    made changes

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,35 +6,38 @@ const About = () => {
   return (
     <div className="about-us">
       <Navbar />
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-center py-8 mt-10">
-        {/* Left Side - Image */}
-        <div className="w-full md:w-1/2 mb-4 md:mb-0">
-          <img
-            src="https://t4.ftcdn.net/jpg/06/50/75/23/360_F_650752340_svHnkTNmdl7ISCeJMRrnyu9ZfDXLAkGT.webp"
-            alt="Placeholder"
-            className="w-full h-auto rounded-lg shadow-md"
-          />
-        </div>
+      <div data-aos="zoom-in">
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-center py-8 mt-10">
+          {/* Left Side - Image */}
+          <div className="w-full md:w-1/2 mb-4 md:mb-0">
+            <img
+              src="https://t4.ftcdn.net/jpg/06/50/75/23/360_F_650752340_svHnkTNmdl7ISCeJMRrnyu9ZfDXLAkGT.webp"
+              alt="Placeholder"
+              className="w-full h-auto rounded-lg shadow-md"
+            />
+          </div>
 
-        {/* Right Side - Text */}
-        <div className="w-full md:w-1/2 md:ml-8 mt-4 md:mt-0">
-          <h2 className="text-3xl font-bold mb-4">About beatha business</h2>
-          <p className="text-gray-700 leading-relaxed">
-            Welcome to Beatha Business where we provide quality products to our
-            customers. We offer a variety of products including oils and
-            perfumes. Our popular products include{" "}
-            <span className="text-primary">QIE Paris lotion</span>,{" "}
-            <span className="text-primary">QIE Paris perfume</span>,{" "}
-            <span className="text-primary">
-              PR Francoise Bredon Paris lotion
-            </span>
-            , and{" "}
-            <span className="text-primary">
-              PR Francoise Bredon Paris perfume
-            </span>
-            . This is our main product menu. We welcome you all to our platform
-            to enjoy our quality products. We offer oils for both men and women.
-          </p>
+          {/* Right Side - Text */}
+          <div className="w-full md:w-1/2 md:ml-8 mt-4 md:mt-0">
+            <h2 className="text-3xl font-bold mb-4">About beatha business</h2>
+            <p className="text-gray-700 leading-relaxed">
+              Welcome to Beatha Business where we provide quality products to
+              our customers. We offer a variety of products including oils and
+              perfumes. Our popular products include{" "}
+              <span className="text-primary">QIE Paris lotion</span>,{" "}
+              <span className="text-primary">QIE Paris perfume</span>,{" "}
+              <span className="text-primary">
+                PR Francoise Bredon Paris lotion
+              </span>
+              , and{" "}
+              <span className="text-primary">
+                PR Francoise Bredon Paris perfume
+              </span>
+              . This is our main product menu. We welcome you all to our
+              platform to enjoy our quality products. We offer oils for both men
+              and women.
+            </p>
+          </div>
         </div>
       </div>
       <Footer />
--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -7,102 +7,7 @@ const Checkout = () => {
     <>
       <Navbar />
       <div data-aos="zoom-in">
-        {/* <div className="checkout flex mt-10" style={{ marginLeft: "20rem" }}>
-          <div className="left-side flex-none mr-4">
-            <img
-              src="https://media.istockphoto.com/id/1671142169/vector/web-phishing-illustration-on-mobile-concept.jpg?s=612x612&w=0&k=20&c=XDfAqPBBiHFZCbEqUZognNbY4XTD-XTpacgjmH-Q-sA="
-              alt="Cyclist"
-              className="h-full object-cover"
-            />
-          </div>
-          <div className="right-side max-w-md mx-auto p-6 shadow-md w-96 ml-4">
-            <h1 className="text-2xl font-bold mb-4">BILLING ADDRESS</h1>
-            <form>
-              <div className="mb-4">
-                <label htmlFor="country" className="block font-medium mb-1">
-                  Country
-                </label>
-                <input
-                  type="text"
-                  id="country"
-                  name="country"
-                  required
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="town" className="block font-medium mb-1">
-                  Town
-                </label>
-                <input
-                  type="text"
-                  id="town"
-                  name="town"
-                  required
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="streetAddress"
-                  className="block font-medium mb-1"
-                >
-                  Street Address
-                </label>
-                <input
-                  type="text"
-                  id="streetAddress"
-                  name="streetAddress"
-                  required
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="state" className="block font-medium mb-1">
-                  State
-                </label>
-                <input
-                  type="text"
-                  id="state"
-                  name="state"
-                  required
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="postcode" className="block font-medium mb-1">
-                  Postcode
-                </label>
-                <input
-                  type="text"
-                  id="postcode"
-                  name="postcode"
-                  required
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="email" className="block font-medium mb-1">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  required
-                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                />
-              </div>
-              <button
-                type="submit"
-                className="bg-primary text-white px-4 py-2 rounded-md "
-              >
-                Continue
-              </button>
-            </form>
-          </div>
-        </div> */}
-
+      
         <div className="checkout flex flex-col md:flex-row mt-10 md:mx-auto md:max-w-4xl">
           <div className="left-side flex-none md:w-1/2 mr-4 mb-4 md:mb-0">
             <img
@@ -193,7 +98,7 @@ const Checkout = () => {
                 type="submit"
                 className="bg-primary text-white w-full py-2 rounded-md hover:bg-primary"
               >
-                Continue
+               Place order
               </button>
             </form>
           </div>
