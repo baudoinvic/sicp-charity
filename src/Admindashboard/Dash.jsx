52e23e6525e7b4ab897be312a2d27f4e01f6ad0a
baudoin <baudoinvicbolingo@gmail.com>
2024-03-21 18:33:55 +0200

    added dash page

--- src/Admindashboard/Dash.jsx
+++ src/Admindashboard/Dash.jsx
@@ -1,39 +1,63 @@
 import React from 'react'
+import {
+  HiUserGroup,
+  HiUser,
+  HiShoppingCart,
+  HiCurrencyDollar,
+} from "react-icons/hi";
 
 function Dash() {
   return (
     <div>
-      <div className="notification">
-        <h2 className="dashboard-title">Dashboard</h2>
-        <div className="not">
-          <img
-            className="third-logo-admin"
-            src="https://www.shutterstock.com/image-vector/user-icon-trendy-flat-style-600nw-418179856.jpg"
-            alt=""
-            draggable="false"
-          />
-          <span className="">Admin</span>
-          {/* <NotificationsNoneIcon /> */}
-        </div>
+      <div class="bg-gray-200 py-4">
+        <h2 class="text-2xl font-semibold text-gray-800 text-center">
+          Dashboard
+        </h2>
       </div>
 
-      <div className="card-container">
-        <div className="card">
-          <span className="workload">Workload</span>
-          <h2>34.416</h2>
-          <p>time of working per day</p>
+      <div className="flex flex-wrap justify-center mt-8">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mx-4 mb-8">
+          <div className="px-10 py-4">
+            <div className="flex items-center mb-4">
+              <HiUserGroup className="w-8 h-8 text-blue-500 mr-2" />
+              <div className="font-bold text-xl text-blue-500">Customers</div>
+            </div>
+            <h2 className="text-3xl text-gray-800 font-semibold">34,416</h2>
+            <p className="text-gray-700 text-base">Our customers</p>
+          </div>
         </div>
 
-        <div className="card">
-          <span className="facility">Facility</span>
-          <h2>10.4p</h2>
-          <p>number of treating patients</p>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mx-4 mb-8">
+          <div className="px-10 py-4">
+            <div className="flex items-center mb-4">
+              <HiUser className="w-8 h-8 text-green-500 mr-2" />
+              <div className="font-bold text-xl text-green-500">Users</div>
+            </div>
+            <h2 className="text-3xl text-gray-800 font-semibold">100</h2>
+            <p className="text-gray-700 text-base">Number of users</p>
+          </div>
         </div>
 
-        <div className="card">
-          <span className="facility">Salary</span>
-          <h2>$20.4k</h2>
-          <p>calculating the Salary per year</p>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mx-4 mb-8">
+          <div className="px-10 py-4">
+            <div className="flex items-center mb-4">
+              <HiShoppingCart className="w-8 h-8 text-blue-500 mr-2" />
+              <div className="font-bold text-xl text-blue-500">Orders</div>
+            </div>
+            <h2 className="text-3xl text-gray-800 font-semibold">20.4k</h2>
+            <p className="text-gray-700 text-base">Number of orders</p>
+          </div>
+        </div>
+
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mx-4 mb-8">
+          <div className="px-10 py-4">
+            <div className="flex items-center mb-4">
+              <HiCurrencyDollar className="w-8 h-8 text-blue-500 mr-2" />
+              <div className="font-bold text-xl text-blue-500">Income</div>
+            </div>
+            <h2 className="text-3xl text-black font-semibold">$20.4k</h2>
+            <p className="text-black text-base">Total income</p>
+          </div>
         </div>
       </div>
     </div>
--- src/Admindashboard/Dashboard.jsx
+++ src/Admindashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
- import { NavLink, Outlet } from "react-router-dom";
+ import { Link, NavLink, Outlet } from "react-router-dom";
  import { IoMdHome } from "react-icons/io";
  import { MdProductionQuantityLimits } from "react-icons/md";
  import { FaUserGroup } from "react-icons/fa6";
@@ -20,42 +20,44 @@ function Dashboard() {
           <ul className="py-4">
             <li className="px-4 py-4 flex items-center font-bold">
               <IoMdHome className="mr-4" />
-              <span className='cursor-pointer'>Home</span>
+              <span className="cursor-pointer">Home</span>
             </li>
             <li className="px-4 py-4 flex items-center font-bold">
               <MdProductionQuantityLimits className="mr-4" />
-              <span className='cursor-pointer'>Products</span>
+              <span className="cursor-pointer">Products</span>
             </li>
             <li className="px-4 py-4 flex items-center font-bold">
               <FaUserGroup className="mr-4" />
-              <span className='cursor-pointer'>Customers</span>
+              <span className="cursor-pointer">Customers</span>
             </li>
             <li className="px-4 py-4 flex items-center font-bold">
               <FaUserPlus className="mr-4" />
-              <span className='cursor-pointer'>Users</span>
+              <span className="cursor-pointer">Users</span>
             </li>
             <li className="px-4 py-4 flex items-center font-bold">
               <FaBorderStyle className="mr-4" />
-              <span className='cursor-pointer'>Orders</span>
+              <span className="cursor-pointer">Orders</span>
             </li>
             <li className="px-4 py-4 flex items-center font-bold">
               <IoSettingsOutline className="mr-4" />
-              <span className='cursor-pointer'>Settings</span>
+              <span className="cursor-pointer">Settings</span>
             </li>
           </ul>
           <div className="mt-auto px-4 py-2 flex font-bold">
             <span className="block cursor-pointer hover:text-gray-300 flex items-center">
               <TbLogout className="mr-4 font-bold" />
-              <span className='cursor-pointer'>Logout</span>
+              <Link to="/Login">
+                <span className="cursor-pointer">Logout</span>
+              </Link>
             </span>
-           
           </div>
         </div>
 
         {/* Content */}
         <div className="flex-1 bg-gray-200 p-8">
-          {/* This is where you render the content */}
-          {/* Make sure to use Outlet from react-router to render nested routes */}
+          <div className="sidebar-right-side">
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
