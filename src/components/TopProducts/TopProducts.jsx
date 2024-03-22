f6b4e593b7ecb426117aff1a8cf2e32aee55e649
baudoin <baudoinvicbolingo@gmail.com>
2024-03-22 17:21:37 +0200

    change order now to cart

--- src/components/TopProducts/TopProducts.jsx
+++ src/components/TopProducts/TopProducts.jsx
@@ -78,7 +78,7 @@ const TopProducts = ({ handleOrderPopup }) => {
                     className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary"
                     onClick={handleOrderPopup}
                   >
-                    Order Now
+                    Add to cart
                   </button>
                 </Link>
               </div>
