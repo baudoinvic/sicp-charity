9daa9fe0862081079c28122495ee3d6c106c5c4f
baudoin <baudoinvicbolingo@gmail.com>
2024-03-29 01:23:17 +0200

    done set

--- src/Admindashboard/Setting.jsx
+++ src/Admindashboard/Setting.jsx
@@ -2,17 +2,44 @@ import React from 'react'
 
 const Setting = () => {
   return (
-    <div className="setting">
-      <div className="left-side">
-        <h1>Personal information</h1>
-        <input type="firstname" text="name" placeholder="firstname"></input>
-        <input type="email" text="email" placeholder="email"></input>
-        <input type="adress" text="address" placeholder="address"></input>
+    <div className="flex justify-between">
+      <div className="w-1/2">
+        <h1 class="text-lg font-bold mb-4">Personal information</h1>
+        <input
+          type="text"
+          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
+          placeholder="First Name"
+        ></input>
+        <br />
+        <input
+          type="email"
+          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
+          placeholder="Email"
+        ></input>
+        <br></br>
+        <br />
+        <input
+          type="text"
+          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
+          placeholder="Address"
+        ></input>
       </div>
-      <div className="right-side">
-        <input type="lastname" text="lastname" placeholder="lastname"></input>
-        <input type="phone" text="phone" placeholder="phone number"></input>
-        <button>Save Changes</button>
+      <div className="w-1/2">
+        <input
+          type="text"
+          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
+          placeholder="Last Name"
+        ></input>
+        <br />
+        <input
+          type="tel"
+          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
+          placeholder="Phone Number"
+        ></input>
+        <br />
+        <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Save Changes
+        </button>
       </div>
     </div>
   );
