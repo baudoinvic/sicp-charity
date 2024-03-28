96f64ab9db557edabdbabcf2e7c98277de35fd7c
baudoin <baudoinvicbolingo@gmail.com>
2024-03-29 01:12:20 +0200

    made changes

--- src/Admindashboard/Setting.jsx
+++ src/Admindashboard/Setting.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 const Setting = () => {
   return (
-    <div className='setting'>
-        
+    <div className="setting">
+      <div className="left-side">
+        <h1>Personal information</h1>
+        <input type="firstname" text="name" placeholder="firstname"></input>
+        <input type="email" text="email" placeholder="email"></input>
+        <input type="adress" text="address" placeholder="address"></input>
+      </div>
+      <div className="right-side">
+        <input type="lastname" text="lastname" placeholder="lastname"></input>
+        <input type="phone" text="phone" placeholder="phone number"></input>
+        <button>Save Changes</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Setting
