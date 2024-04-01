ff7349b711ac4e7a62ed97d9c7f8bd3270d28c65
baudoin <baudoinvicbolingo@gmail.com>
2024-04-01 16:00:29 +0200

    fix settings page

--- src/Admindashboard/Setting.jsx
+++ src/Admindashboard/Setting.jsx
@@ -1,129 +1,122 @@
-import React from 'react'
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 
 const Setting = () => {
   return (
     <div className="setting-user">
-      <div className="settings-tab">
-       
-      </div>
-
-      <div className="information-part">
-        <div className="setting-left-side">
-          <div className="change-information">
-            <span className="update-profile">
-             
-              update profile
-            </span>
-            <span className="security">
-            
-              security
-            </span>
-            <span className="password-change">
-              {/* <FontAwesomeIcon icon={faKey} /> */}
-              change password
-            </span>
-          </div>
-        </div>
-        <div className="setting-right-side">
-          <h4 className="profile-setting-tile">profile</h4>
-          <span className="setting-info-title">
+      <div className="information-part flex">
+        <div className="setting-right-side ml-16">
+          <h4 className="profile-setting-tile mt-16 ml-8">profile</h4>
+          <span className="setting-info-title ml-8 text-gray-700">
             This information will be displayed publicly be careful what you
             share
           </span>
-          <form className="settings-tab-form">
-            <div className="row">
-              <div className="col">
+          <form className="settings-tab-form ml-8 mt-16">
+            <div className="row flex gap-4">
+              <div className="col flex-1">
                 <input
                   type="text"
                   id="name"
                   name="name"
                   placeholder="Enter your name"
+                  className="w-full p-2 border border-gray-300 mb-4"
                 />
               </div>
-              <div className="col">
+              <div className="col flex-1">
                 <input
                   type="email"
                   id="email"
                   name="email"
-                  placeholder="enter your email"
+                  placeholder="Enter your email"
+                  className="w-full p-2 border border-gray-300 mb-4"
                 />
               </div>
             </div>
 
-            <div className="row">
-              <div className="col">
-                <select id="gender" name="gender">
+            <div className="row flex gap-4">
+              <div className="col flex-1">
+                <select
+                  id="gender"
+                  name="gender"
+                  className="w-full p-2 border border-gray-300 mb-4"
+                >
                   <option value="gender">Gender</option>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                   <option value="other">Other</option>
                 </select>
               </div>
-              <div className="col">
+              <div className="col flex-1">
                 <input
                   type="tel"
                   id="phone"
                   name="phone"
-                  placeholder="your phone number"
+                  placeholder="Your phone number"
+                  className="w-full p-2 border border-gray-300 mb-4"
                 />
               </div>
             </div>
 
-            <span className="personal-address">personal address</span>
+            <span className="personal-address mr-8 mt-10">personal address</span>
+   
 
-            <div className="row">
-              <div className="col">
+            <div className="row flex gap-4">
+              <div className="col flex-1">
                 <input
                   type="text"
                   id="country"
-                  name="name"
-                  placeholder="your country"
+                  name="country"
+                  placeholder="Your country"
+                  className="w-full p-2 border border-gray-300 mb-4"
                 />
               </div>
-              <div className="col">
+              <div className="col flex-1">
                 <input
                   type="text"
                   id="province"
-                  name="name"
-                  placeholder="province"
+                  name="province"
+                  placeholder="Province"
+                  className="w-full p-2 border border-gray-300 mb-4"
                 />
               </div>
             </div>
 
-            <div className="row">
-              <div className="col">
-                <div className="col">
-                  <input
-                    type="text"
-                    id="District"
-                    name="name"
-                    placeholder="District"
-                  />
-                </div>
+            <div className="row flex gap-4">
+              <div className="col flex-1">
+                <input
+                  type="text"
+                  id="district"
+                  name="district"
+                  placeholder="District"
+                  className="w-full p-2 border border-gray-300 mb-4"
+                />
               </div>
-              <div className="col">
+              <div className="col flex-1">
                 <input
                   type="tel"
                   id="phone"
                   name="phone"
-                  placeholder="your phone number"
+                  placeholder="Your phone number"
+                  className="w-full p-2 border border-gray-300 mb-4"
                 />
               </div>
             </div>
           </form>
 
-          <div className="cancel">
-            <button className="canceling">Cancel</button>
+          <div className="cancel flex items-center p-5 ml-4">
+            <button className="canceling py-4 px-10 border border-black mr-4">
+              Cancel
+            </button>
             <div className="save">
-              <button className="saving">Save</button>
+              <button className="saving py-4 px-10 bg-purple-700 text-white border border-white">
+                Save
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Setting
+export default Setting;
