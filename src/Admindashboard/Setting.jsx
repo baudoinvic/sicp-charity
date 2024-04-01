469fafabc4b9ee8fa0a4fad6309071217acc8ffb
baudoin <baudoinvicbolingo@gmail.com>
2024-04-01 15:48:37 +0200

    change the setting page

--- src/Admindashboard/Setting.jsx
+++ src/Admindashboard/Setting.jsx
@@ -1,45 +1,126 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const Setting = () => {
   return (
-    <div className="flex justify-between items-center mt-20">
-      <div className="w-1/2 mr-5">
-        <h1 class="text-lg font-bold mb-4">Personal information</h1>
-        <input
-          type="text"
-          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
-          placeholder="First Name"
-        ></input>
-        <br />
-        <input
-          type="email"
-          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
-          placeholder="Email"
-        ></input>
-   
-        <br />
-        <input
-          type="text"
-          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
-          placeholder="Address"
-        ></input>
+    <div className="setting-user">
+      <div className="settings-tab">
+       
       </div>
-      <div className="w-1/2">
-        <input
-          type="text"
-          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
-          placeholder="Last Name"
-        ></input>
-        <br />
-        <input
-          type="tel"
-          class="w-full bg-gray-100 border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:border-blue-500"
-          placeholder="Phone Number"
-        ></input>
-        <br />
-        <button class="bg-primary text-white font-bold py-2 px-4 rounded ">
-          Save Changes
-        </button>
+
+      <div className="information-part">
+        <div className="setting-left-side">
+          <div className="change-information">
+            <span className="update-profile">
+             
+              update profile
+            </span>
+            <span className="security">
+            
+              security
+            </span>
+            <span className="password-change">
+              {/* <FontAwesomeIcon icon={faKey} /> */}
+              change password
+            </span>
+          </div>
+        </div>
+        <div className="setting-right-side">
+          <h4 className="profile-setting-tile">profile</h4>
+          <span className="setting-info-title">
+            This information will be displayed publicly be careful what you
+            share
+          </span>
+          <form className="settings-tab-form">
+            <div className="row">
+              <div className="col">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Enter your name"
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="enter your email"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col">
+                <select id="gender" name="gender">
+                  <option value="gender">Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              <div className="col">
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  placeholder="your phone number"
+                />
+              </div>
+            </div>
+
+            <span className="personal-address">personal address</span>
+
+            <div className="row">
+              <div className="col">
+                <input
+                  type="text"
+                  id="country"
+                  name="name"
+                  placeholder="your country"
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  id="province"
+                  name="name"
+                  placeholder="province"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col">
+                <div className="col">
+                  <input
+                    type="text"
+                    id="District"
+                    name="name"
+                    placeholder="District"
+                  />
+                </div>
+              </div>
+              <div className="col">
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  placeholder="your phone number"
+                />
+              </div>
+            </div>
+          </form>
+
+          <div className="cancel">
+            <button className="canceling">Cancel</button>
+            <div className="save">
+              <button className="saving">Save</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
