ee902b1a801c15cdbde2d2f6668200f04c4d9219
baudoin <baudoinvicbolingo@gmail.com>
2024-04-01 17:47:08 +0200

    fixing product tab

--- src/Admindashboard/Product.jsx
+++ src/Admindashboard/Product.jsx
@@ -16,9 +16,10 @@ const Product = () => {
         </div>
       </div>
       <div class="flex ml-5 mt-10">
-        <span class=" mr-64">johnsonema</span>
-        <span class=" mr-64">ema</span>
-        <span class=" mr-64">emma@gmail.com</span>
+        <span class=" mr-64">1</span>
+        <span class=" mr-64">Bredon</span>
+        <span class=" mr-64">12</span>
+        <span class=" mr-64">12$</span>
         <div class="flex items-center">
           <FaEdit className="text-2xl text-gray-900" />
           <MdDeleteOutline className="ml-4 text-2xl text-red-500" />
