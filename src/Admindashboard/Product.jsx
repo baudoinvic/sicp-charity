349e5de4b4adbcf1cab1a94a3cee717892147c71
baudoin <baudoinvicbolingo@gmail.com>
2024-04-05 13:21:31 +0200

    fetched all produdcts

--- src/Admindashboard/Product.jsx
+++ src/Admindashboard/Product.jsx
@@ -1,74 +1,3 @@
-// import React from "react";
-// import { MdDeleteOutline } from "react-icons/md";
-// import { FaEdit } from "react-icons/fa";
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-// import { ToastContainer, toast } from "react-toastify";
-
-// const Product = () => {
-//   const [Products, setProducts] = useState([]);
-
-//   const fetchProducts = () => {
-//     let token = localStorage.getItem("token");
-//     axios({
-//       url: "https://beathaecommerceback-end.onrender.com/api/v1/product/viewAllProd",
-//       method: "GET",
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     })
-//       .then((response) => {
-//         const productsData = response.data.products; // Access the products array from the response
-//         setProducts(productsData);
-//         toast.success(response.data.message);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching products:", error);
-//         toast.error("Error fetching products. Please try again later.");
-//       });
-//   };
-
-//   useEffect(() => {
-//     fetchProducts();
-//   }, []);
-
-//   return (
-//     <div data-aos="zoom-in" className="">
-//       <span className="text-3xl font-bold">Products</span>
-//       <div className="bg-white p-4 rounded-lg mt-10">
-//         <div className="flex shadow-md mt-10">
-//           <span className="font-bold w-1/4">Productname</span>
-//           <span className="font-bold w-1/4">Price</span>
-//           <span className="font-bold w-1/4">Stock quantity</span>
-//           <span className="font-bold w-1/4">Category</span>
-//           <span className="font-bold w-1/4">Action</span>
-//         </div>
-//         {Products.map((product) => (
-//           <div className="flex mt-10" key={product._id}>
-//             <span className="w-1/4">{product.productName}</span>
-//             <span className="w-1/4">{product.price}</span>
-//             <span className="w-1/4">{product.stock_quantity}</span>
-//             <span className="w-1/4">{product.category}</span>
-//             <div className="w-1/4 flex items-center">
-//               <FaEdit
-//                 className="text-2xl text-gray-900 mr-2"
-//                 style={{ cursor: "pointer" }}
-//               />
-//               <MdDeleteOutline
-//                 className="text-2xl text-red-500"
-//                 style={{ cursor: "pointer" }}
-//               />
-//               <ToastContainer />
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Product;
-
 
 import React, { useState } from "react";
 import { MdDeleteOutline } from "react-icons/md";
@@ -79,7 +8,7 @@ import { ToastContainer, toast } from "react-toastify";
 
 const Product = () => {
   const [Products, setProducts] = useState([]);
-  const [file, setFile] = useState(null); // State for the selected file
+ 
 
   const fetchProducts = () => {
     let token = localStorage.getItem("token");
@@ -143,7 +72,7 @@ const Product = () => {
                 className="text-2xl text-red-500"
                 style={{ cursor: "pointer" }}
               />
-              <ToastContainer />
+            
             </div>
           </div>
         ))}
