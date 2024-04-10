47f65b1024b7fd404d73ef20b1fc94b25b848c41
baudoin <baudoinvicbolingo@gmail.com>
2024-04-10 11:07:39 +0200

    fetched

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -22,7 +22,7 @@ const Subscribe = () => {
       <div className="container backdrop-blur-sm py-10">
         <div className="space-y-6 max-w-xl mx-auto">
           <h1 className="text-2xl !text-center sm:text-left sm:text-4xl  font-semibold">
-            Get Notified About New Product
+            Get Notified About New Products
           </h1>
           <div className="flex flex-col sm:flex-row sm:items-center text-gray-500 gap-4">
             <input
