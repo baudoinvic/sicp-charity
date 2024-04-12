36e61a41367a2ba0896af76f9e89a95a7f790c5a
baudoin <baudoinvicbolingo@gmail.com>
2024-04-12 13:20:05 +0200

    today commit

--- src/Admindashboard/Order.jsx
+++ src/Admindashboard/Order.jsx
@@ -37,42 +37,7 @@ const Order = () => {
 
 
   return (
-    // <div data-aos="zoom-in" className="mt-10">
-    //   <span className="text-3xl font-bold">orders</span>
-    //   <div class="bg-white p-4 rounded-lg ">
-    //     <div class="flex shadow-md mt-10">
-    //       <span class="font-bold w-1/4">customer name</span>
-    //       <span class="font-bold w-1/4">order date</span>
-    //       <span class="font-bold w-1/4">Amount</span>
-    //       <span class="font-bold w-1/4">Payment status</span>
-    //       <span class="font-bold w-1/4">Action</span>
-    //     </div>
-
-    //     <div className="flex mt-10">
-    //       <span className="w-1/4"></span>
-    //       <span className="w-1/4"></span>
-    //       <span className="w-1/4"></span>
-
-    //       <div className="w-1/4 flex items-center">
-    //         <FaEdit
-    //           className="text-2xl text-gray-900 mr-2"
-    //           style={{ cursor: "pointer" }}
-    //         />
-    //         <MdDeleteOutline
-    //           className="text-2xl text-red-500"
-    //           style={{ cursor: "pointer" }}
-    //         />
-    //         <span
-    //           className="w-1/4 bg-green-500 py-1 px-2 rounded-lg text-white text-center"
-    //           style={{ cursor: "pointer" }}
-    //         >
-    //           View
-    //         </span>
-    //       </div>
-
-    //     </div>
-    //   </div>
-    // </div>
+    
 
     <div data-aos="zoom-in" className="mt-10">
       <span className="text-3xl font-bold">Orders</span>
