8188ec6a357dd9ea4ea6b64330151726f73d3f10
baudoin <baudoinvicbolingo@gmail.com>
2024-07-18 15:20:35 +0200

    updated event

--- src/Event/Event.jsx
+++ src/Event/Event.jsx
@@ -45,22 +45,23 @@ const Event = () => {
   return (
     <div>
       <Navbar />
-      <div className="min-h-screen bg-gradient-to-br py-8">
-        <div className="container mx-auto p-4 max-w-4xl">
-          <h1 className="text-4xl font-bold mb-8 text-center text-primary">
-            Events and Newsletters
-          </h1>
+      <div className="min-h-screen bg-gradient-to-br py-8 px-4 sm:px-10">
+        <h1 className="text-3xl sm:text-4xl font-bold mb-8 text-left">
+          Upcoming
+        </h1>
 
-          {/* Upcoming Events Section */}
-          <section className="mb-12">
-            <h2 className="text-3xl font-bold mb-6 text-primary border-b-2 border-primary pb-2">
-              Upcoming Events
-            </h2>
-            <div className="bg-white mb-4 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-              <h3 className="text-2xl font-semibold mb-3 text-indigo-600 w-2/4 text-primary">
-                On 20 July 2024: Visiting Unaccompanied Patients at Hospitals
-              </h3>
-              <p className="text-gray-700 mb-4 leading-relaxed">
+        {/* Events Section */}
+        <div className="flex flex-col lg:flex-row justify-center py-12 space-y-8 lg:space-y-0 lg:space-x-4">
+          {/* Upcoming Events */}
+          <div className="w-full lg:w-1/4">
+            <div className="flex flex-col items-start">
+              <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48">
+                <div className="bg-primary text-white p-4 text-center h-full flex flex-col justify-center">
+                  <h2 className="text-2xl font-bold mb-2">Upcoming Event</h2>
+                  <span className="text-lg">20 July 2024</span>
+                </div>
+              </div>
+              <p className="text-gray-700 leading-relaxed w-full lg:w-2/3 text-sm">
                 We plan to visit and assist the unaccompanied patients at MUHIMA
                 Hospital. We will help the patients to take showers and change
                 to new clothes. We will also give those words of encouragement
@@ -69,117 +70,135 @@ const Event = () => {
                 medications to those patients who are unable to afford hospital
                 bills.
               </p>
-           
             </div>
-          </section>
+          </div>
 
-          {/* Past Events Section */}
-          <section className="mb-12">
-            <h2 className="text-3xl font-bold mb-6 text-primary border-b-2 border-primary pb-2">
-              Past Events
-            </h2>
-            <div className="bg-white mb-4 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-              <h3 className="text-2xl font-semibold mb-3 text-primary w-2/4">
-                On 27 April 2024: Provision of School Supplies to Children and
-                Youth
-              </h3>
-              <p className="text-gray-700 mb-4 leading-relaxed">
-                Planning to provide 20 children and 10 students in the secondary
-                school the urgent and basic materials needed to fit in academia
-                life such as pens, uniforms, notebooks, tickets, and school
-                fees.
-              </p>
-            
-            </div>
-          </section>
-
-          {/* Newsletters Section */}
-          <section>
-            <h2 className="text-3xl font-bold mb-6 text-primary border-b-2 border-primary pb-2">
-              Newsletters
-            </h2>
-
-            {/* Daily Newsletters */}
-            <div className="bg-white mb-8 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-              <h3 className="text-2xl font-semibold mb-3 text-primary ">
-                Daily: Environmental Courses
-              </h3>
-              <p className="text-gray-700 mb-2 font-medium">
+          <div className="w-full lg:w-1/4">
+            <div className="flex flex-col items-start">
+              <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48">
+                <div className="bg-primary text-white p-4 text-center h-full flex flex-col justify-center">
+                  <h2 className="text-2xl font-bold mb-2">
+                    Environmental Courses
+                  </h2>
+                  <span className="text-lg">10 August 2024</span>
+                </div>
+              </div>
+              <p className="w-full lg:w-2/3 font-bold">
                 The Importance of Preserving and Conserving Green Grassland
               </p>
-              <p className="text-gray-700 mb-4 leading-relaxed">
+              <p className="text-gray-700 leading-relaxed w-full lg:w-2/3 mt-3 text-sm">
                 Globally, over-utilization of resources and climate change have
                 caused increasingly serious ecological damage, including but not
                 limited to losses of biodiversity and ecosystem stability,
                 threatening the sustainability of ecological functions and
                 services.
               </p>
-            
             </div>
+          </div>
 
-            {/* Weekly Newsletters */}
-            <div className="bg-white mb-8 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-              <h3 className="text-2xl font-semibold mb-3 text-primary ">
-                Weekly: Social Studies
-              </h3>
-              <p className="text-gray-700 mb-2 font-medium w-2/4">
+          <div className="w-full lg:w-1/4">
+            <div className="flex flex-col items-start">
+              <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48">
+                <div className="bg-primary text-white p-4 text-center h-full flex flex-col justify-center">
+                  <h2 className="text-2xl font-bold mb-2">
+                    Weekly Social Studies
+                  </h2>
+                  <span className="text-lg">25 August 2024</span>
+                </div>
+              </div>
+              <p className="w-full lg:w-2/3 font-bold">
                 Cooperatives As A Path Way to The Women Sustainable Livelihoods
                 and Empowerment
               </p>
-              <p className="text-gray-700 mb-4 leading-relaxed">
+              <p className="text-gray-700 leading-relaxed w-full lg:w-2/3 mt-3 text-sm">
                 Over the past two centuries cooperatives had been introduced to
                 the world and ever since, the term cooperative has been
                 developed in various form in different sectors in which human
                 survival activities take places.
               </p>
-           
             </div>
+          </div>
 
-            {/* Monthly Newsletters */}
-            <div className="bg-white mb-8 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-              <h3 className="text-2xl font-semibold mb-3 text-primary ">
-                Monthly: Social Studies
-              </h3>
-              <p className="text-gray-700 mb-2 font-medium">
+          <div className="w-full lg:w-1/4">
+            <div className="flex flex-col items-start">
+              <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48">
+                <div className="bg-primary text-white p-4 text-center h-full flex flex-col justify-center">
+                  <h2 className="text-2xl font-bold mb-2">
+                    Monthly Social Studies
+                  </h2>
+                  <span className="text-lg">27 April 2024</span>
+                </div>
+              </div>
+              <p className="w-full lg:w-2/3 font-bold">
                 Cooperatives in Pandemics, Food Security, War conflict and
                 Climate Issues in Africa
               </p>
-              <p className="text-gray-700 mb-4 leading-relaxed">
+              <p className="text-gray-700 text-sm leading-relaxed w-full lg:w-2/3 mt-3">
                 Cooperatives have an important role in increasing community
                 resilience to climate change, wars, conflicts, and pandemics.
               </p>
-         
             </div>
-
-            {/* Subscription Form */}
-            <div className="bg-white mb-8 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-              <h3 className="text-2xl font-semibold mb-3 text-primary">
-                Subscribe to our Newsletter
-              </h3>
-              <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                  <input
-                    type="email"
-                    placeholder="Your Email Address"
-                    className="w-full p-2 border border-indigo-200 rounded focus:outline-none focus:ring-2 focus:ring-indigo-400"
-                    required
-                    value={email}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div>
-                  <button
-                    type="submit"
-                    className="bg-primary hover:bg-indigo-700 text-white py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-indigo-400"
-                  >
-                    Subscribe
-                  </button>
-                </div>
-              </form>
-            </div>
-            <ToastContainer />
-          </section>
+          </div>
         </div>
+
+        <h2 className="text-3xl font-bold mb-6 text-primary border-b-2 border-primary pb-2"></h2>
+        <h1 className="text-3xl sm:text-4xl font-bold mb-8 text-left">
+          Past Events
+        </h1>
+
+        <section className="flex justify-start py-12">
+          <div className="flex flex-col items-start max-w-md">
+            <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48">
+              <div className="bg-gray-100 text-black p-4 text-center h-full flex flex-col justify-center">
+                <h2 className="text-2xl font-bold mb-2">Past Events</h2>
+                <span className="text-lg">27 April 2024</span>
+              </div>
+            </div>
+            <div className="text-gray-700 leading-relaxed text-sm">
+              <p className="font-bold mb-2">
+                Provision of School Supplies to Children and Youth
+              </p>
+              <p>
+                We plan to visit and assist the unaccompanied patients at MUHIMA
+                Hospital. We will help the patients to take showers and change
+                to new clothes. We will also give those words of encouragement
+                and hope to help them not to feel lonely and left. Furthermore,
+                we will provide foods, snacks, and some pocket money to buy
+                medications to those patients who are unable to afford hospital
+                bills.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div className="bg-white mb-8 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 w-full sm:w-2/3 lg:w-2/6">
+          <h3 className="text-2xl font-semibold mb-3 text-primary">
+            Subscribe to our Newsletter
+          </h3>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <input
+                type="email"
+                id="email"
+                placeholder="Your Email Address"
+                className="w-full p-2 border border-indigo-200 rounded focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                required
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full bg-primary hover:bg-indigo-700 text-white py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              >
+                Subscribe
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <ToastContainer />
       </div>
       <Footer />
     </div>
