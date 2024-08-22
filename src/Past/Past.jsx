22bfd37f2556ea24bdbca0b543d8037c9e29972f
baudoin <baudoinvicbolingo@gmail.com>
2024-08-22 12:58:58 +0200

    added another file

---
+++ src/Past/Past.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Past = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <div className="">
+        <h1>Visiting Unaccompanied Patients at Hospitals</h1>
+        <p>
+          We plan to visit and Assist the unaccompanied patients at MUHIMA
+          Hospital. We will help the patients to take showers and change to new
+          clothes. We will also give those words of encouragement and hope to
+          help them not to feel lonely and left. Furthermore we will provide
+          foods, snacks, and some pocket money to buy medications to those
+          patients who are unable to afford hospital bills.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Past
