52c58b625a0d15e9be47ff91e91180e90fd09444
baudoin <baudoinvicbolingo@gmail.com>
2024-08-22 12:55:34 +0200

    added new file

---
+++ src/Month/Month.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Month = () => {
+  return (
+    <div className="monthly">
+      <Navbar />
+      <div className="">
+        <h1>
+          Cooperatives in Pandemics, Food Security, War conflict and Climate
+          Issues in Africa
+        </h1>
+
+        <p>
+          Cooperatives in Pandemics, Food Security, War conflict and Climate
+          Issues in Africa Cooperatives have an important role in increasing
+          community resilience to climate change, wars, conflicts, and
+          pandemics. According to the seventh principle of cooperatives,
+          "Concern for Community," cooperatives should have concern for not just
+          their shareholders but in addition the community around them during
+          times of crisis. The latest exceptional global issues, including as
+          the COVID-19 pandemic, the Ukraine-Russia war, international
+          conflicts, environmental destruction, and the energy and food
+          collapse, have had a negative impact on society and necessitate
+          ongoing joint responses. African cooperatives have helped to
+          facilitate climate change adaptation and mitigation by supporting,
+          among other things, the Kyoto Protocol and the Paris Agreement on
+          greenhouse gas reductions (ICA, 2023). Cooperatives are more connected
+          to people's needs and have systems and processes in place that can
+          identify climate change victims and provide targeted adaptation and
+          mitigation strategies. The cooperative movement engages its members
+          and prospective members in sustainable resource management and the
+          selection of suitable projects.
+        </p>
+
+        <p>
+          Concerning pandemics, the cooperative movement promotes the creation
+          of robust measures to mitigate the disastrous impacts of pandemics.
+          Several countries implemented stringent steps to combat the spread of
+          COVID-19, particularly during the period when it was most prevalent.
+          These initiatives had varying effects on people's livelihoods and
+          different sectors of the African economy. Among other things, the
+          effects included lower demand and supply of goods and services, higher
+          adoption of internet-based services, less productivity in agriculture,
+          higher levels of food insecurity, and the shutdown of businesses.
+          Notwithstanding these consequences, numerous cooperatives continued to
+          operate while offering humanitarian assistance to their members and
+          nearby populations (ICA, 2023).
+        </p>
+
+        <p>
+          {" "}
+          On the other side, certain African countries are still experiencing
+          instability as a result of conflicts and wars. These wars have
+          resulted in the collapse or reduction of corporate activities,
+          displacement, low social cohesiveness, inadequate leadership
+          structures and procedures, widespread poverty, joblessness, and
+          inequality, all of which contribute to unrest and destabilization.
+          Governments and cooperative movements in war-torn nations have since
+          shifted their focus to mediation and reconciliation (ICA, 2023). In
+          this context, the African cooperative movement as a social and
+          commercial model focuses on reducing risk for marginalised and
+          disadvantaged populations by facilitating social protection efforts
+          and identifying reliable and appropriate business prospects.
+          Furthermore, they seek to bring back peace in conflict zones. Another
+          major issue confronting most African countries is food insecurity.
+          There are several cooperatives in Africa, particularly agricultural
+          ones that seek to promote food security for not only their members but
+          additionally those nearby (ICA, 2023). More importantly, the African
+          continent is packed with resources that have never been used to their
+          full potential including fertile and virgin lands, water, and human
+          resources for which cooperatives can utilize to make difference in
+          food security issues on the continent.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Month
---
+++ src/Social/Social.jsx
@@ -0,0 +1,139 @@
+import React from 'react'
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+const Social = () => {
+  return (
+    <div className="">
+      <Navbar />
+
+      <div className="">
+        <h1>
+          Cooperatives As A Path Way to The Women Sustainable Livelihoods and
+          Empowerment
+        </h1>
+
+        <p>
+          Over the past two centuries cooperatives had been introduced to the
+          world and ever since, the term cooperative has been developed in
+          various form in different sectors in which human survival activities
+          take places, meaning the environment of socio-economic and political
+          aspect. They are usually an association of organized groups of people,
+          organizations, or countries who/which share same values and
+          objectives, willing to work together in order to achieve a defined or
+          shared vision. The roots of cooperatives historically are traced in
+          Europe in 19th century as their origin before they spread to other
+          developed countries. Undoubtedly, the creation and growth of these
+          cooperatives was portrayed as a tool to eradicate the delicate
+          situation of absolute poverty and to advance country’s economy. It is
+          simply and normal to find cooperatives in different sectors like
+          agriculture, mineral, industries and service sectors as they are
+          entrusted in performing an excellent role of enhancing the livelihoods
+          of rural residents in many developing countries.
+        </p>
+
+        <p>
+          Kenya as one of African countries to have cooperatives’ development
+          long history in comparison to other sub-Saharan and east African
+          countries like Rwanda, has proven cooperatives to be the driver of its
+          growth, and indicated that they have made the main contributions to
+          the overall economy after its independence. In 2009, the Kenya
+          Ministry of Development and Marketing confirmed that 80% of Kenya’s
+          citizen was earning its income from cooperatives’ practices directly
+          or indirectly. In 2004, it was highlighted that in Africa; not less
+          than 40% of the families are affiliated in cooperative communities
+          (Schwettmann, 2004). In Rwanda particularly there are 10,981
+          cooperatives registered currently with 5,293,090 members (Rwanda
+          Cooperative Agency, 2022) who make also the 40 percent of the whole
+          Rwanda population of 13 Million (World Bank, 2022). In this respect
+          the cooperative movement is regarded to be the continent important
+          non-
+        </p>
+
+        <p>
+          governmental organisation (NGO). An International Labour Organisation
+          (ILO) reports in 1998 highlighted that cooperatives lift up a huge
+          package in economic growth and job creation promotion. This because
+          cooperatives are production companies in which their members create
+          their own employment, as well as services’ providers to their members.
+          This signifies that cooperatives can also be influential and plays a
+          crucial role in many state economies of developing countries
+          especially in Africa. Based on the statistics of ILO study in 1997, it
+          was estimated that in 15 African countries the cooperative sector was
+          the main source of around 160000 direct jobs created (Wanyama,
+          Develtere & Pollet 2008), later on in 2015 cooperatives have
+          accomplished a significant contribution to the economy of Africa by
+          forging 5.6 million jobs and 2.1 million cooperative led occupations
+          (ICA, 2022). This indicates that cooperatives avail opportunities in
+          job creation which is also really important in poverty reduction and
+          sustainable livelihood for the cooperatives members, who may get
+          direct wage employment from secondary and primary cooperatives as well
+          as those who earn decent income from the self-employment generated
+          through cooperatives.
+        </p>
+
+        <p>
+          However cooperatives can also still create jobs to cooperative
+          non-member through spillover effect of cooperatives’ activities based
+          on transactions and opportunities created by cooperatives that is
+          additionally crucial to the overall country economy. Despites the
+          opportunity of job creation, the importance and contribution of
+          cooperatives in forging jobs have been forsaken by cooperative
+          promoter agencies, planners of jobs, social partners and non profit
+          organisation (Schwettmann, 2004). Nevertheless, when considering the
+          context of Rwanda which is one of the sub-Saharan land-locked
+          countries in central east Africa. In this small hilly country,
+          Cooperatives arose from the colonial government’s efforts to establish
+          structures for policy implementation. Cooperatives were still used
+          after independence, as the new established government accepted them as
+          instruments for enacting policies and development objectives. This
+          autonomous government, in particular, saw cooperatives as institutions
+          that would meet the subsistence requirements of its stakeholders
+          instead of being seen as economic entities (Espérance Mukarugwiza &
+          ILO, 2010). This placed cooperative members to governmental control,
+          impeding the development of cooperatives as economic entities that
+          suit the needs and interests of its members. The period of civil wars
+          from 1990 to 1994 in Rwanda further hampered cooperatives growth by
+          undermining all socioeconomic activities in the country.
+        </p>
+
+        <p>
+          However after 1994, the new government took over and set structural
+          transformation in various sectors of socio-economic and political
+          activities that had resulted into boom of many economic spheres as
+          well as high increase in cooperatives creation in Rwanda. This is
+          evidenced by statistics from Rwanda Cooperative Agency (2022) which
+          reported that in Rwanda there are a total number of 10,981 registered
+          cooperatives in which agriculture sector covers 45.9%, services sector
+          covers 16.6%, the trading sector cover 15.3%, and handicraft covers
+          10.8% while the financial sector and other remaining sectors cover
+          11.4%. It is further reported that cooperatives play a significant
+          role in poverty reduction, creating jobs, access to market, food
+          security, building capacity and the overall development of Rwandans.
+          In addition they are seen as the best model to achieve financial
+          inclusion and other socio-economic development aspects. It is evident
+          that all the aforementioned facts from cooperatives intervention
+          combined together can be the source of rural sustainable livelihood in
+          gender context when considering the disadvantaged population in rural
+          areas especially women who rarely participate in many of development
+          activities based on various facts including extreme poverty, being
+          blocked by cultural and social right in they are silenced by high
+          pressure of men surroundings as well inability to economic access.
+          Such assumption of cooperatives potentials might be supported by the
+          case of India as an example, in which the country’s first women
+          cooperative federation has risen from 33 to 103 member cooperatives of
+          various types and sizes where member cooperatives’ collective income
+          has levelled up 295% since 2005, and 65,985 women have improved and
+          increased livelihoods whereas in country like Philippines 1.4 million
+          individuals and over 7.2 million dependants are safeguarded under
+          micro insurance policies and among these policyholders more than 70%
+          are women (Jo‐Anne Ferguson, 2012) who frequently perceived to be
+          marginalized.
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Social
