76cfde661d12ce2f64e9f36229129fe386200c76
baudoin <baudoinvicbolingo@gmail.com>
2024-08-28 17:04:54 +0200

    fix it

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -11,13 +11,14 @@ const News = () => {
         <Navbar />
         <div className="min-h-screen bg-gradient-to-br py-8 px-4 sm:px-10">
           <h1 className="text-3xl sm:text-4xl font-bold mb-8 text-left">
-            Ours NewsLetters
+           Current Newsletter
           </h1>
 
           <div className="flex flex-col lg:flex-row justify-center py-12 space-y-8 lg:space-y-0 lg:space-x-4">
+           
             <div className="w-full lg:w-1/4">
               <div className="flex flex-col items-start">
-                <Link to="/New">
+                <Link to="/Current">
                   <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
                     <div className="bg-primary text-white p-4 text-center h-full flex flex-col justify-center">
                       <span className="text-lg ">
@@ -27,23 +28,25 @@ const News = () => {
                   </div>
                 </Link>
 
-                <p className="leading-relaxed w-full lg:w-2/3 text-sm text-blue-500 underline">
-                  Current Newsletter.
-                  <br /> End of summer Vacation and New academic years In
-                  September,
-                </p>
+                <Link to="/Current">
+                  <p className="leading-relaxed w-full lg:w-2/3 text-sm text-blue-500 underline">
+                    Current Newsletter.
+                    <br /> End of summer Vacation and New academic years In
+                    September,
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
 
           <h2 className="text-3xl font-bold mb-6 text-primary border-b-2 border-primary pb-2"></h2>
           <h1 className="text-3xl sm:text-4xl font-bold mb-8 text-left">
-            Old/Past Newsletter
+            Old Past Newsletter
           </h1>
 
           <section className="flex justify-start py-12">
             <div className="flex flex-col items-start max-w-md">
-              <Link to="/Past">
+              <Link to="/Old">
                 <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
                   <div className="bg-gray-100 text-black p-4 text-center h-full flex flex-col justify-center">
                     <span className="text-1xl font-bold mb-2">
@@ -54,7 +57,7 @@ const News = () => {
               </Link>
 
               <div className="text-gray-700 leading-relaxed text-sm">
-                <Link to="/Past">
+                <Link to="/Old">
                   <p className="w-full lg:w-2/3 font-bold text-blue-500 underline ">
                     Old/Past Newsletter about Christmas
                   </p>
