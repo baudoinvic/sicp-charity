c4acc819512588db4c25e98a33f3a45983aef9b8
baudoin <baudoinvicbolingo@gmail.com>
2024-08-29 14:21:31 +0200

    display pdf file in browser

--- src/News/Current.jsx
+++ src/News/Current.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar';
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 const Current = () => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   return (
     <>
       <div className="current">
         <Navbar />
+        <div style={{ height: "750px", marginTop: "20px" }}>
+          <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
+          >
+            <Viewer
+              fileUrl="/current.pdf"
+              plugins={[defaultLayoutPluginInstance]}
+            />
+          </Worker>
+        </div>
       </div>
     </>
   );
-}
+};
 
-export default Current
+export default Current;
