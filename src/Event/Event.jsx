1478407bcfe3d8ff89a03d9c19f8eb116b4db1c6
baudoin <baudoinvicbolingo@gmail.com>
2024-08-30 21:01:03 +0200

    remove needed code

--- src/Event/Event.jsx
+++ src/Event/Event.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import axios from "axios";
-import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 
