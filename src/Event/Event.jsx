047644381c2281b19b1b3bc808d6816e2888ea41
baudoin <baudoinvicbolingo@gmail.com>
2024-08-31 19:20:33 +0200

    delete toast

--- src/Event/Event.jsx
+++ src/Event/Event.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 
 const Event = () => {
