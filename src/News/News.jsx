cdf1b69c79f6bdaf6d71a9651870ba31b211c829
baudoin <baudoinvicbolingo@gmail.com>
2024-09-02 13:39:28 +0200

    trying to fix the newsletter page

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -13,7 +13,7 @@ const News = () => {
           <h1 className="text-3xl sm:text-4xl font-bold mb-8 text-left">
             Current Newsletter
           </h1>
-          <div className="flex flex-col lg:flex-row justify-start py-12 space-y-8 lg:space-y-0 lg:space-x-4">
+          <div className="flex space-x-4">
             <div className="w-full lg:w-1/4">
               <div className="flex flex-col items-start">
                 <Link to="/Current">
@@ -35,7 +35,75 @@ const News = () => {
                 </Link>
               </div>
             </div>
+
+            <div className="w-full lg:w-1/4">
+              <div className="flex flex-col items-start">
+                <Link to="/Current">
+                  <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
+                    <div className="bg-primary text-white p-4 text-center h-full flex flex-col justify-center">
+                      <span className="text-lg">
+                        Current Newsletter – August 2024
+                      </span>
+                    </div>
+                  </div>
+                </Link>
+
+                <Link to="/Current">
+                  <p className="leading-relaxed w-full text-sm text-blue-500">
+                    Merry Christmas and Happy New Year 2025
+                  </p>
+                </Link>
+              </div>
+            </div>
           </div>
+
+          {/* <div className="">
+            <div className="">
+              <div className="w-full lg:w-1/4">
+                <div className="flex flex-col items-start">
+                  <Link to="/Current">
+                    <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
+                      <div className="bg-primary text-white p-4 text-center h-full flex flex-col justify-center">
+                        <span className="text-lg">
+                          Current Newsletter – August 2024
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+
+                  <Link to="/Current">
+                    <p className="leading-relaxed w-full text-sm text-blue-500">
+                      Current Newsletter.
+                      <br /> End of summer Vacation and New academic years In
+                      September,
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col lg:flex-row justify-start py-12 space-y-8 lg:space-y-0 lg:space-x-4">
+              <div className="w-full lg:w-1/4">
+                <div className="flex flex-col items-start">
+                  <Link to="/Current">
+                    <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
+                      <div className="bg-primary text-white p-4 text-center h-full flex flex-col justify-center">
+                        <span className="text-lg">
+                          Current Newsletter – August 2024
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+
+                  <Link to="/Current">
+                    <p className="leading-relaxed w-full text-sm text-blue-500">
+                      Merry Christmas and Happy New Year 2025
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div> */}
 
           <h2 className="text-3xl font-bold mb-6 text-primary border-b-2 border-primary pb-2"></h2>
           <h1 className="text-3xl sm:text-4xl font-bold mb-8 text-left">
@@ -48,7 +116,7 @@ const News = () => {
                 <div className="bg-white shadow-lg overflow-hidden mb-4 w-full max-w-[12rem] h-48 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
                   <div className="bg-gray-100 text-black p-4 text-center h-full flex flex-col justify-center">
                     <span className="text-1xl font-bold mb-2">
-                      Merry Christmas and Happy New Year 2025
+                      Past Newsletter about Christmas
                     </span>
                   </div>
                 </div>
