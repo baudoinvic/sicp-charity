5195a1df88bea43e0300503b47b8117fabd3baad
baudoin <baudoinvicbolingo@gmail.com>
2024-09-23 16:15:29 +0200

    trying

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -127,16 +127,7 @@ const Navbar = ({ handleOrderPopup }) => {
           </div>
 
           <div className="flex justify-between items-center gap-4">
-            <div className="relative group hidden sm:block">
-              <input
-                type="text"
-                placeholder="Search"
-                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800"
-              />
-              <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
-            </div>
-           
-
+          
             <Link to="/Login">
               <div className="sign-in flex">
                 <FaRegUser className="mt-2" />
